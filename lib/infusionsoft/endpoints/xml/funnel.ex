441b88bf25b59c808e2368b4d1cafc270fb122bf
Damon Janis <damonvjanis@gmail.com>
2018-12-19 23:55:22 -0700

    Spec and document core XML API functions for Funnel actions

--- lib/infusionsoft/endpoints/xml/funnel.ex
+++ lib/infusionsoft/endpoints/xml/funnel.ex
@@ -1,11 +1,14 @@
 defmodule Infusionsoft.Endpoints.XML.Funnel do
   @moduledoc """
-  Provides the endpoints to Infusionsoft's XML API for Funnel actions.
+  Provides the raw endpoints to Infusionsoft's XML API for Funnel actions.
   """
-  alias Infusionsoft.Endpoints.XML
+  alias Infusionsoft.Endpoints.XML.Helpers
 
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#funnel-achieve-a-goal"
+  @spec achieve_goal(integer(), String.t(), String.t(), String.t()) ::
+          {:ok, list()} | {:error, String.t()}
   def achieve_goal(contact_id, integration_name, call_name, token) do
     params = ["", integration_name, call_name, contact_id]
-    XML.process_endpoint("FunnelService.achieveGoal", params, token)
+    Helpers.process_endpoint("FunnelService.achieveGoal", params, token)
   end
 end
