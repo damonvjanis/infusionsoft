cdbcbeec41c92eefa7bdb43501f10697001fce42
Damon Janis <damonvjanis@gmail.com>
2018-12-26 13:20:44 -0700

    Remove comments

--- lib/infusionsoft/application.ex
+++ lib/infusionsoft/application.ex
@@ -1,18 +1,13 @@
 defmodule Infusionsoft.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      Infusionsoft.Caches.ContactCustomFields
+      Infusionsoft.Caches.ContactCustomFields,
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Infusionsoft.Supervisor]
     Supervisor.start_link(children, opts)
   end
