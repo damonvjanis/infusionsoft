b1ccb7e6072e64c94d79cafffa94a1fcceb011e4
Damon Janis <damonvjanis@gmail.com>
2018-12-29 00:11:51 -0700

    Add recursive helper function for data table query

--- lib/infusionsoft/endpoints/xml/data.ex
+++ lib/infusionsoft/endpoints/xml/data.ex
@@ -27,4 +27,40 @@ defmodule Infusionsoft.Endpoints.XML.Data do
 
     Helpers.process_endpoint("DataService.query", params, token, app)
   end
+
+  @doc """
+  Helper to recurse and get the full number of records instead of one page
+
+  https://developer.infusionsoft.com/docs/xml-rpc/#data-query-a-data-table
+
+  Available options:
+  order_by - defualts to Id
+  ascending - defaults to false
+  """
+  @spec query_all_from_table(
+          map(),
+          String.t(),
+          [String.t()],
+          String.t(),
+          nil | String.t(),
+          keyword()
+        ) :: {:ok, list()} | {:error, String.t()}
+  def query_all_from_table(query, table, r_fields, token, app, opts \\ []) do
+    do_query_all_from_table(query, table, r_fields, token, app, opts, 0, [], [])
+  end
+
+  defp do_query_all_from_table(_, _, _, _, _, _, count, acc, []) when count != 0 do
+    {:ok, acc}
+  end
+
+  defp do_query_all_from_table(query, table, r_fields, token, app, opts, count, acc, new) do
+    opts = Keyword.merge(opts, page: count, limit: 1000)
+
+    with {:ok, next} <- query_table(query, table, r_fields, token, app, opts) do
+      current = acc ++ new
+      count = count + 1
+      opts = Keyword.merge(opts, page: count, limit: 1000)
+      do_query_all_from_table(query, table, r_fields, token, app, opts, count, current, next)
+    end
+  end
 end
