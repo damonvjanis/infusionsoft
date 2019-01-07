897e75ed8c9b3a27c09a3bb9a15d65bf87234e76
Damon Janis <damonvjanis@gmail.com>
2019-01-07 16:44:02 -0700

    Add the rest of the endpoints for Data

--- lib/infusionsoft/endpoints/xml/data.ex
+++ lib/infusionsoft/endpoints/xml/data.ex
@@ -63,4 +63,116 @@ defmodule Infusionsoft.Endpoints.XML.Data do
       do_query_all_from_table(query, table, r_fields, token, app, opts, count, current, next)
     end
   end
+
+  @doc """
+  https://developer.infusionsoft.com/docs/xml-rpc/#data-find-a-record-by-matching-a-specific-field
+
+  Available options:
+  page - defaults to 0
+  limit - defaults to 1000
+  """
+  @spec find_by_field(
+          String.t(),
+          String.t(),
+          String.t(),
+          [String.t()],
+          String.t(),
+          nil | String.t(),
+          keyword()
+        ) :: {:ok, list()} | {:error, String.t()}
+  def find_by_field(value, field, table, return_fields, token, app, opts \\ []) do
+    opts = Keyword.merge([page: 0, limit: 1000], opts)
+    page = Keyword.fetch!(opts, :page)
+    limit = Keyword.fetch!(opts, :limit)
+
+    params = Helpers.build_params([table, limit, page, field, value, return_fields], token, app)
+
+    Helpers.process_endpoint("DataService.findByField", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-create-a-record"
+  @spec create_record(map(), String.t(), String.t(), nil | String.t()) ::
+          {:ok, integer()} | {:error, String.t()}
+  def create_record(data, table, token, app \\ nil) do
+    params = Helpers.build_params([table, data], token, app)
+    Helpers.process_endpoint("DataService.add", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-retrieve-a-record"
+  @spec retrieve_record(integer(), String.t(), [String.t()], String.t(), nil | String.t()) ::
+          {:ok, map()} | {:error, String.t()}
+  def retrieve_record(id, table, fields, token, app \\ nil) do
+    params = Helpers.build_params([table, id, fields], token, app)
+    Helpers.process_endpoint("DataService.load", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-update-a-record"
+  @spec update_record(map(), integer(), String.t(), String.t(), nil | String.t()) ::
+          {:ok, integer()} | {:error, String.t()}
+  def update_record(data, id, table, token, app \\ nil) do
+    params = Helpers.build_params([table, id, data], token, app)
+    Helpers.process_endpoint("DataService.update", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-delete-a-record"
+  @spec delete_record(integer(), String.t(), String.t(), nil | String.t()) ::
+          {:ok, boolean()} | {:error, String.t()}
+  def delete_record(id, table, token, app \\ nil) do
+    params = Helpers.build_params([table, id], token, app)
+    Helpers.process_endpoint("DataService.delete", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-count-a-data-table-s-records"
+  @spec count_records(map(), String.t(), String.t(), nil | String.t()) ::
+          {:ok, integer()} | {:error, String.t()}
+  def count_records(data, table, token, app \\ nil) do
+    params = Helpers.build_params([table, data], token, app)
+    Helpers.process_endpoint("DataService.count", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-create-a-custom-field"
+  @spec create_custom_field(
+          String.t(),
+          String.t(),
+          String.t(),
+          integer(),
+          String.t(),
+          nil | String.t()
+        ) :: {:ok, integer()} | {:error, String.t()}
+  def create_custom_field(name, table, type, header, token, app \\ nil) do
+    params = Helpers.build_params([table, name, type, header], token, app)
+    Helpers.process_endpoint("DataService.addCustomField", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-update-a-custom-field"
+  @spec update_custom_field(integer(), map(), String.t(), nil | String.t()) ::
+          {:ok, boolean()} | {:error, String.t()}
+  def update_custom_field(id, data, token, app \\ nil) do
+    params = Helpers.build_params([id, data], token, app)
+    Helpers.process_endpoint("DataService.updateCustomField", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-retrieve-an-appointment-s-icalendar-file"
+  @spec retrieve_appt_ical(integer(), String.t(), nil | String.t()) ::
+          {:ok, String.t()} | {:error, String.t()}
+  def retrieve_appt_ical(id, token, app \\ nil) do
+    params = Helpers.build_params([id], token, app)
+    Helpers.process_endpoint("DataService.getAppointmentICal", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-retrieve-application-setting"
+  @spec retrieve_app_settings(String.t(), String.t(), String.t(), nil | String.t()) ::
+          {:ok, String.t()} | {:error, String.t()}
+  def retrieve_app_settings(module, setting, token, app \\ nil) do
+    params = Helpers.build_params([module, setting], token, app)
+    Helpers.process_endpoint("DataService.getAppSetting", params, token, app)
+  end
+
+  @doc "https://developer.infusionsoft.com/docs/xml-rpc/#data-validate-a-user-s-credentials"
+  @spec authenticate_user(String.t(), String.t(), String.t(), nil | String.t()) ::
+          {:ok, integer() | String.t()} | {:error, String.t()}
+  def authenticate_user(username, password_hash, token, app \\ nil) do
+    params = Helpers.build_params([username, password_hash], token, app)
+    Helpers.process_endpoint("DataService.authenticateUser", params, token, app)
+  end
 end
