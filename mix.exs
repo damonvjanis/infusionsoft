283989470db4ed8e922cfa4deba70d68bb6dbf74
Damon Janis <damonvjanis@gmail.com>
2019-12-31 22:09:03 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Infusionsoft.MixProject do
   def project do
     [
       app: :infusionsoft,
-      version: "0.6.0",
+      version: "0.6.1",
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
